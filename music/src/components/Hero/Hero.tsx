6228b172b0467a08b336c5be08cd7682c1c633e1
Vikram Snehi <20402559+vsnehi@users.noreply.github>
2026-07-26 02:46:24 +0530

    new web

---
+++ music/src/components/Hero/Hero.tsx
@@ -0,0 +1,11 @@
+import styles from './Hero.module.scss';
+
+const Hero = () => {
+  return (
+    <section className={styles.hero}>
+      <h1>Snehi Music</h1>
+    </section>
+  );
+};
+
+export default Hero;
